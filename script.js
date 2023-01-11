fd93d3e73f478d6db6d8bd6aa1cfbd430832b498
Giovani Jimenez <68931942+giovanijimenez@users.noreply.github.com>
2023-01-10 19:20:00 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let productoUno = prompt("Producto Uno");
+let productoDos = ("Producto Dos");
+let productoTres = document.getElementById (console.log("Funciona DOM"));
+
+console.log("FUNCIONA");
+
+if(productoUno == 1){
+    console.log("Añadiste producto 1");
+}else if (productoDos == 2){
+    console.log("Añadiste producto 2");
+}else if (prductoTres == 3){
+    console.log("añadiste prducto 3");
+}else{
+    console.log("No agregaste");
+}
