27553bb739805be0d81947246ee364d6a067e518
Giovani Jimenez <68931942+giovanijimenez@users.noreply.github.com>
2023-02-07 21:57:13 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let serieTres = document.getElementById ("serieTres");
 let teramont = document.getElementById ("teramont");
 let mustang = document.getElementById ("mustang");
 
-serieTres.onclick = () => {console.log("agregaste BMW serie Tres"); alert("Agregaste bmw")}
+serieTres.onclick = () => {console.log("agregaste BMW serie Tres");}
 teramont.onclick = () => {console.log("agregaste VW teramont");}
 mustang.onclick = () => {console.log("agregaste Ford mustang")}
 
@@ -13,5 +13,47 @@ let sleccionAuto = document.getElementById("seleccionado");
 console.log(seleccionado.innerHTML);
 
 //Cambio seleccion
-seleccionado.innerHTML = "serieTres"
+seleccionado.innerHTML = "bmw serie 3"
 console.log(seleccionado.innerHTML);
+
+
+
+const ejCrear = document.getElementById("ejCrear");
+
+const ejeCrear = document.createElement("p");
+
+
+console.log(ejeCrear);
+
+
+
+//json
+const serie3 = {
+    id: 1,
+    modelo: "Serie 3",
+    precio: 950000,
+};
+const teramontVW = {
+    id: 2,
+    modelo: "Teramont",
+    precio: 857490,
+};
+const mustangFord = {
+    id: 3,
+    modelo: "Mustang",
+    precio: 1236900,
+};
+
+localStorage.setItem("serie3", serie3);
+localStorage.setItem("teramont", teramont);
+localStorage.setItem("mustang", mustang);
+
+
+
+const productoJson = JSON.stringify(serie3);
+
+console.log(productoJson);
+console.log(typeof serie3);
+console.log(typeof productoJson);
+
+localStorage.setItem("serie3", productoJson)
